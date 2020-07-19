32b682c7072b8046ddda8073f8e735fdfa7c6ae0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-19 11:37:15 +0000

    Deploy WeiDongjunGabriel/resume to github.com/WeiDongjunGabriel/resume.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/avatar.jpg","7286d66787dbb03cc8eb3a33d2c35161"],["/css/page.css","cd5ca9ac172a3b66201d4312492a5840"],["/css/style.min.css","4e24d76f09a7bc5616f9f58cd6a63c47"],["/css/vue-pdf.css","1da002040a79305451bab39f30b37567"],["/favicon.png","7b46983716ea1a2ec50114a5e13c30dd"],["/index.html","e9c92b184a8e1eddfd04633b7dab1e01"],["/js/common.js","748b78854064e842c09d1b9605209bba"],["/js/css.escape.js","a2185e4074b2acf55b190d49300dc626"],["/js/smooth-scroll.min.js","53a7fcc785e987d5ed08302f36de6653"]];
+var precacheConfig = [["/avatar.jpg","7286d66787dbb03cc8eb3a33d2c35161"],["/css/page.css","cd5ca9ac172a3b66201d4312492a5840"],["/css/style.min.css","4e24d76f09a7bc5616f9f58cd6a63c47"],["/css/vue-pdf.css","1da002040a79305451bab39f30b37567"],["/favicon.png","7b46983716ea1a2ec50114a5e13c30dd"],["/index.html","7462f934347ea59c9035ec169292de87"],["/js/common.js","748b78854064e842c09d1b9605209bba"],["/js/css.escape.js","a2185e4074b2acf55b190d49300dc626"],["/js/smooth-scroll.min.js","53a7fcc785e987d5ed08302f36de6653"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
